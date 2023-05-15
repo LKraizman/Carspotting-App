e032ed0ba8e73d38d3046bcdec397281944f9f3b
LKraizman <131805269+LKraizman@users.noreply.github.com>
2023-05-15 20:51:40 +0300

    Very first Spotted Car entity initialisation. The main fields have been created. The addition of the date is implemented through generation when the object is created. The variables carManufacture and carModel must then be selected from a list.

---
+++ src/main/java/com/carspottingapp/spottedCar/CarSpot.java
@@ -0,0 +1,89 @@
+package com.carspottingapp.spottedCar;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class CarSpot {
+    @Id
+    @SequenceGenerator(
+            name = "carspot_id_sequence",
+            sequenceName = "carspot_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "carspot_id_sequence"
+    )
+    private Long carSpotId;
+    private String carSpotTitle;
+    private String carManufacture;
+    private String carModel;
+    private LocalDateTime spotDate;
+
+    public CarSpot(Long id,
+                   String title,
+                   String carManufacture,
+                   String carModel) {
+        this.carSpotId = id;
+        this.carSpotTitle = title;
+        this.carManufacture = carManufacture;
+        this.carModel = carModel;
+        this.spotDate = LocalDateTime.now();
+    }
+
+    public CarSpot() {
+
+    }
+
+    public Long getId() {
+        return carSpotId;
+    }
+
+    public void setId(Long id) {
+        this.carSpotId = id;
+    }
+
+    public String getTitle() {
+        return carSpotTitle;
+    }
+
+    public void setTitle(String title) {
+        this.carSpotTitle = title;
+    }
+
+    public String getCarManufacture() {
+        return carManufacture;
+    }
+
+    public void setCarManufacture(String carManufacture) {
+        this.carManufacture = carManufacture;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public LocalDateTime getSpotDate() {
+        return spotDate;
+    }
+
+    public void setSpotDate(LocalDateTime spotDate) {
+        this.spotDate = spotDate;
+    }
+
+    @Override
+    public String toString() {
+        return "SpottedCar{" +
+                "id=" + carSpotId +
+                ", title='" + carSpotTitle + '\'' +
+                ", carManufacture='" + carManufacture + '\'' +
+                ", carModel='" + carModel + '\'' +
+                ", spotDate='" + spotDate + '\'' +
+                '}';
+    }
+}
